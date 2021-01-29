0dfa118bf7465897a3085cb7925e654c5a091b16
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2021-01-29 10:06:40 +0100

    fix script

--- publish.sh
+++ publish.sh
@@ -9,7 +9,15 @@ die () {
 echo $1 | grep -E -q '^[0-9]+\.[0-9]+(\.[0-9]+)$' || die "Semantic Version argument required, $1 provided"
 
 export TAG=$1
+
+echo "tagging"
+
+git tag $TAG
+
 echo "publishing $TAG"
 
 gradle  -Pgroup=com.github.jillesvangurp -Pversion=$TAG publish
 rsync -azp  localrepo/* jillesvangurpcom@ftp.jillesvangurp.com:/srv/home/jillesvangurpcom/domains/jillesvangurp.com/htdocs/www/maven
+
+
+git push --tags
