73527601b8cd9705116325eaebde283b38a43fac
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2023-11-03 13:26:55 +0100

    warnings

--- src/commonTest/kotlin/com/jillesvangurp/geojson/GeoJsonExtensionsTest.kt
+++ src/commonTest/kotlin/com/jillesvangurp/geojson/GeoJsonExtensionsTest.kt
@@ -32,7 +32,7 @@ class GeoJsonExtensionsTest {
 
     @Test
     fun shouldCreateTriangle() {
-        val t = GeoGeometry.circle2polygon(3, rosenthalerPlatz.latitude, rosenthalerPlatz.longitude, 20.0)
+        GeoGeometry.circle2polygon(3, rosenthalerPlatz.latitude, rosenthalerPlatz.longitude, 20.0)
             .polygonGeometry().let {
                 println(it)
             }
@@ -59,13 +59,13 @@ class GeoJsonExtensionsTest {
         var cs = rectangle.coordinates!!
         GeoGeometry.distance(cs[0][0], cs[0][1]).roundToLong() shouldBe 70
         GeoGeometry.distance(cs[0][1], cs[0][2]).roundToLong() shouldBe 70
-        cs =rectangle.scaleX(30.0)!!.coordinates!!
+        cs = rectangle.scaleX(30.0).coordinates!!
         GeoGeometry.distance(cs[0][0], cs[0][1]).roundToLong() shouldBe (70.0*0.3).toLong()
-        cs =rectangle.scaleX(130.0)!!.coordinates!!
+        cs = rectangle.scaleX(130.0).coordinates!!
         GeoGeometry.distance(cs[0][0], cs[0][1]).roundToLong() shouldBe (70.0*1.3).toLong()
-        cs =rectangle.scaleY(30.0)!!.coordinates!!
+        cs = rectangle.scaleY(30.0).coordinates!!
         GeoGeometry.distance(cs[0][1], cs[0][2]).roundToLong() shouldBe (70.0*0.3).toLong()
-        cs =rectangle.scaleY(130.0)!!.coordinates!!
+        cs = rectangle.scaleY(130.0).coordinates!!
         GeoGeometry.distance(cs[0][1], cs[0][2]).roundToLong() shouldBe (70.0*1.3).toLong()
     }
 
@@ -75,11 +75,11 @@ class GeoJsonExtensionsTest {
         val centroid = rectangle.centroid()
         var cs = rectangle.coordinates!!
         cs[0].forEach { it.distanceTo(centroid).roundToLong() shouldBe 49 }
-        cs  = rectangle.rotate(45.0)!!.coordinates!!
+        cs  = rectangle.rotate(45.0).coordinates!!
         cs[0].forEach { it.distanceTo(centroid).roundToLong() shouldBe 49 }
-        cs  = rectangle.rotate(360.0)!!.coordinates!!
+        cs  = rectangle.rotate(360.0).coordinates!!
         cs[0].forEach { it.distanceTo(centroid).roundToLong() shouldBe 49 }
-        cs  = rectangle.rotate(2000.0)!!.coordinates!!
+        cs  = rectangle.rotate(2000.0).coordinates!!
         cs[0].forEach { it.distanceTo(centroid).roundToLong() shouldBe 49 }
     }
 
@@ -95,12 +95,12 @@ class GeoJsonExtensionsTest {
             Geometry.Point(rectangle.coordinates!![0][2]).asFeature(properties = mapOf("marker-color" to "green").toJsonObject()),
             Geometry.Point(rectangle.coordinates!![0][3]).asFeature(properties = mapOf("marker-color" to "purple").toJsonObject()),
 
-            rectangle.scaleX(50.0)!!.asFeature(properties = mapOf("fill" to "blue").toJsonObject()),
-            rectangle.scaleY(50.0)!!.asFeature(properties = mapOf("fill" to "purple").toJsonObject()),
-            rectangle.scaleX(130.0)!!.asFeature(properties = mapOf("fill" to "green").toJsonObject()),
-            rectangle.scaleY(130.0)!!.asFeature(properties = mapOf("fill" to "yellow").toJsonObject()),
-            rectangle.rotate(10.0)!!.asFeature(properties = mapOf("fill" to "brown").toJsonObject()),
-            rectangle.rotate(45.0)!!.asFeature(properties = mapOf("fill" to "grey").toJsonObject()),
+            rectangle.scaleX(50.0).asFeature(properties = mapOf("fill" to "blue").toJsonObject()),
+            rectangle.scaleY(50.0).asFeature(properties = mapOf("fill" to "purple").toJsonObject()),
+            rectangle.scaleX(130.0).asFeature(properties = mapOf("fill" to "green").toJsonObject()),
+            rectangle.scaleY(130.0).asFeature(properties = mapOf("fill" to "yellow").toJsonObject()),
+            rectangle.rotate(10.0).asFeature(properties = mapOf("fill" to "brown").toJsonObject()),
+            rectangle.rotate(45.0).asFeature(properties = mapOf("fill" to "grey").toJsonObject()),
 
         )).let {
             println( Json.Default.encodeToString(it))
