8b586e92de23efca66c039b998e0dfa4358978a8
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2023-11-16 14:59:35 +0100

    fix utm 2 mgrs

--- src/commonMain/kotlin/com/jillesvangurp/geo/mgrs.kt
+++ src/commonMain/kotlin/com/jillesvangurp/geo/mgrs.kt
@@ -1,10 +1,10 @@
 package com.jillesvangurp.geo
 
+import kotlin.math.floor
 import kotlin.math.roundToInt
 
-
-private val eastingLetters = "ABCDEFGHJKLMNPQRSTUVWXYZ" // Excludes I and O
-private val northingLetters = "ABCDEFGHJKLMNPQRSTUV"    // Excludes I and O
+//private val eastingLetters = "ABCDEFGHJKLMNPQRSTUVWXYZ" // Excludes I and O
+//private val northingLetters = "ABCDEFGHJKLMNPQRSTUV"    // Excludes I and O
 
 data class MgrsCoordinate(
     val longitudeZone: Int,
@@ -19,84 +19,108 @@ data class MgrsCoordinate(
     }
 }
 
-/**
- * Half working implementation based of chat gpt. Don't use this.
- *
- * TODO find better implementation
- *
- */
+fun Int.setForZone(): Int {
+    return when(this%6) {
+        0 ->  6
+        1 ->  1
+        2 ->  2
+        3 ->  3
+        4 ->  4
+        5 ->  5
+        else -> error("should not happen")
+    }
+}
+
+private const val BLOCK_SIZE=100000
+
+fun UtmCoordinate.lookupGridLetters(): Pair<Char, Char> {
+    var row=1
+    var n = northing.roundToInt()
+    while(n >= BLOCK_SIZE) {
+        n -= BLOCK_SIZE
+        row++
+    }
+    row %= 20
+
+    var col = 0
+    var e = easting.roundToInt()
+    while(e>= BLOCK_SIZE) {
+        e -= BLOCK_SIZE
+        col++
+    }
+    col %= 8
+
+    val set = longitudeZone.setForZone()
+
+    val l1Set = when(set) {
+        1->"ABCDEFGH"
+        2->"JKLMNPQR"
+        3->"STUVWXYZ"
+        4->"ABCDEFGH"
+        5->"JKLMNPQR"
+        6->"STUVWXYZ"
+        else -> error("should not happen")
+
+    }
+    val l2Set = if(set%2==0) "FGHJKLMNPQRSTUVABCDE" else "ABCDEFGHJKLMNPQRSTUV"
+
+    val actualCol = if(col==0) 7 else col-1
+    val actualRow = if(row==0) 19 else row-1
+    return l1Set[actualCol] to l2Set[actualRow]
+}
+
 fun UtmCoordinate.convertUTMToMGRS(): MgrsCoordinate {
-    val eastingLetter = getEastingLetter(longitudeZone, easting.toInt())
-    val northingLetter = getNorthingLetter(northing.toInt(), latitudeZoneLetter)
+    val (l1,l2) = lookupGridLetters()
 
-    val eastingValue = (easting % 100000).roundToInt()
-    val northingValue = (northing % 100000).roundToInt()
-
+    val mgrsEasting = floor(easting % BLOCK_SIZE).toInt()
+    val mgrsNorthing = floor(northing % BLOCK_SIZE).toInt()
     return MgrsCoordinate(
         longitudeZone,
         latitudeZoneLetter,
-        eastingLetter,
-        northingLetter,
-        eastingValue.toString().padStart(5, '0').toInt(),
-        northingValue.toString().padStart(5, '0').toInt()
+        l1,
+        l2,
+        mgrsEasting,
+        mgrsNorthing
     )
 }
-
-private fun getEastingLetter(zoneNumber: Int, easting: Int): Char {
-    val gridEasting = (easting / 100000)
-    val index = (gridEasting - 1 + (zoneNumber - 1) * 8) % eastingLetters.length
-    return eastingLetters[index]
-}
-
-/**
- * This is wrong.
- */
-private fun getNorthingLetter(northing: Int, latitudeZoneLetter: Char): Char {
-    // Northing value should be adjusted for the false northing if in the southern hemisphere.
-    val adjustedNorthing = if (latitudeZoneLetter < 'N') northing + 10000000 else northing
-    // The northingLetters string starts with 'A' for 0m N, so no need to subtract 1 from the index.
-    val index = (adjustedNorthing / 100000) % northingLetters.length
-    return northingLetters[index]
-}
-
 
 /**
  * Half working implementation based of chat gpt. Don't use this.
  *
  * TODO find better implementation
  */
-fun convertMGRSToUTM(
-    zoneNumber: Int,
-    zoneLetter: Char,
-    gridSquare: String,
-    easting: Int,
-    northing: Int
-): UtmCoordinate {
-    val eastingLetter = gridSquare[0]
-    val northingLetter = gridSquare[1]
+//fun convertMGRSToUTM(
+//    zoneNumber: Int,
+//    zoneLetter: Char,
+//    gridSquare: String,
+//    easting: Int,
+//    northing: Int
+//): UtmCoordinate {
+//    val eastingLetter = gridSquare[0]
+//    val northingLetter = gridSquare[1]
+//
+//    val eastingOffset = getEastingOffset(eastingLetter, zoneNumber)
+//    val northingOffset = getNorthingOffset(northingLetter, zoneNumber, zoneLetter)
+//
+//    val fullEasting = eastingOffset * 100000 + easting
+//    val fullNorthing = northingOffset * 100000 + northing
+//
+//    return UtmCoordinate(zoneNumber, zoneLetter, fullEasting.toDouble(), fullNorthing.toDouble())
+//}
 
-    val eastingOffset = getEastingOffset(eastingLetter, zoneNumber)
-    val northingOffset = getNorthingOffset(northingLetter, zoneNumber, zoneLetter)
-
-    val fullEasting = eastingOffset * 100000 + easting
-    val fullNorthing = northingOffset * 100000 + northing
-
-    return UtmCoordinate(zoneNumber, zoneLetter, fullEasting.toDouble(), fullNorthing.toDouble())
-}
-
-private fun getEastingOffset(eastingLetter: Char, zoneNumber: Int): Int {
-    val eastingIndex = eastingLetters.indexOf(eastingLetter)
-    return (eastingIndex + 1 - (zoneNumber - 1) * 8 + eastingLetters.length) % eastingLetters.length
-}
-
-private fun getNorthingOffset(northingLetter: Char, zoneNumber: Int, zoneLetter: Char): Int {
-    var northingIndex = northingLetters.indexOf(northingLetter)
-    northingIndex = (northingIndex + 1 - (zoneNumber - 1) * 2 + northingLetters.length) % northingLetters.length
-
-    // Adjust for the latitude band
-    val northingBase = "CDEFGHJKLMNPQRSTUVWX".indexOf(zoneLetter.uppercaseChar()) * 8
-    return (northingBase + northingIndex) % northingLetters.length
-}
+//private fun getEastingOffset(eastingLetter: Char, zoneNumber: Int): Int {
+//    val eastingIndex = eastingLetters.indexOf(eastingLetter)
+//    return (eastingIndex + 1 - (zoneNumber - 1) * 8 + eastingLetters.length) % eastingLetters.length
+//}
+//
+//private fun getNorthingOffset(northingLetter: Char, zoneNumber: Int, zoneLetter: Char): Int {
+//    var northingIndex = northingLetters.indexOf(northingLetter)
+//    northingIndex = (northingIndex + 1 - (zoneNumber - 1) * 2 + northingLetters.length) % northingLetters.length
+//
+//    // Adjust for the latitude band
+//    val northingBase = "CDEFGHJKLMNPQRSTUVWX".indexOf(zoneLetter.uppercaseChar()) * 8
+//    return (northingBase + northingIndex) % northingLetters.length
+//}
 
 
 
--- src/commonTest/kotlin/com/jillesvangurp/geogeometry/MGRSTest.kt
+++ src/commonTest/kotlin/com/jillesvangurp/geogeometry/MGRSTest.kt
@@ -2,12 +2,37 @@ package com.jillesvangurp.geogeometry
 
 import com.jillesvangurp.geo.convertUTMToMGRS
 import com.jillesvangurp.geo.toUtmCoordinate
+import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.withClue
+import io.kotest.matchers.shouldBe
 import kotlin.test.Test
 
 class MGRSTest {
     @Test
     fun shouldCalculateMgrsForBrandenburgerTor() {
-        val mgrs = brandenBurgerGate.toUtmCoordinate().convertUTMToMGRS()
-        println(mgrs)
+        assertSoftly {
+            brandenBurgerGate.toUtmCoordinate().convertUTMToMGRS().let {
+                withClue(it) {
+                    it.longitudeZone shouldBe 33
+                    it.latitudeZoneLetter shouldBe 'U'
+                    it.eastingLetter shouldBe 'U'
+                    it.northingLetter shouldBe 'U'
+                    it.easting shouldBe 89880
+                    it.northing shouldBe 19700
+                }
+
+            }
+            val skagen = doubleArrayOf(10.591979, 57.724205)
+            skagen.toUtmCoordinate().convertUTMToMGRS().let {
+                withClue(it) {
+                    it.longitudeZone shouldBe 32
+                    it.latitudeZoneLetter shouldBe 'V'
+                    it.eastingLetter shouldBe 'N'
+                    it.northingLetter shouldBe 'J'
+                    it.easting shouldBe 94817
+                    it.northing shouldBe 99119
+                }
+            }
+        }
     }
 }
