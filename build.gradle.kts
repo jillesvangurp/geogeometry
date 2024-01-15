1bd8611cbf7f654843e2551d4e7d1d3c19e80122
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2024-01-15 17:30:15 +0100

    bump everything to 11. 8 doesn't work apparently.

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,7 +97,7 @@ tasks.withType<KotlinJvmCompile> {
 
     kotlinOptions {
         // this is the minimum LTS version we support, 8 is no longer supported
-        jvmTarget = "8"
+        jvmTarget = "11"
         languageVersion = "1.9"
     }
 }
